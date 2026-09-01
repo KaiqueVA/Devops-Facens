733844eb05ff30f3eccc5b7878653ef297fde0cd
Kaique Vecchia Alves <kaique.alves@facens.br>
2026-09-01 20:44:02 -0300

    chore: estrutura do projeto

---
+++ src/main/java/br/facens/horascomplementares/HorasComplementaresApplication.java
@@ -0,0 +1,12 @@
+package br.facens.horascomplementares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorasComplementaresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorasComplementaresApplication.class, args);
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+-- Catalogo de categorias de atividades complementares e seus tetos (em horas)
+INSERT INTO categoria (nome, teto_horas) VALUES ('ENSINO', 80);
+INSERT INTO categoria (nome, teto_horas) VALUES ('PESQUISA', 80);
+INSERT INTO categoria (nome, teto_horas) VALUES ('EXTENSAO', 80);
+INSERT INTO categoria (nome, teto_horas) VALUES ('EVENTO', 60);
